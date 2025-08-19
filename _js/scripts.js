bf97049bfe23d0e6fb8c308b7c23fd08c66ec778
Matheus G Benevides <matheus.g.benevides@aluno.senai.br>
2025-08-19 14:59:12 -0300

    Alteração Final

--- _js/scripts.js
+++ _js/scripts.js
@@ -3,5 +3,6 @@ document.getElementById("paragrafo2").innerHTML = document.title;
 document.getElementById("imagem").innerHTML = "<img src='../_imagens/jslogo.png' alt='Imagem JS' onclick='msgImg()'>";
 function msgImg() {
     alert('Imagem "logo" do JavaScript, está imagem tem fundo amarelo com JS escrito no canto inferior direito e está com a fonte preta');
+    document.getElementById("textoDaImg").innerHTML='Imagem "logo" do JavaScript, está imagem tem fundo amarelo com JS escrito no canto inferior direito e está com a fonte preta';
 }
-document.getElementById("futuroBotao").innerHTML = "<button type='button' onclick='window.print()'>Imprimir</button>"
+document.getElementById("futuroBotao").innerHTML = "<button type='button' onclick='window.print()'>Imprimir</button>";
